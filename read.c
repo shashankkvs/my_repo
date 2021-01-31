2cb9b94a13775a102631d28986bbdca2895d960f
shashankkvs <shashank.kvs0308@gmail.com>
2021-01-31 20:11:54 +0530

    read file cahnged

--- read.c
+++ read.c
@@ -3,7 +3,7 @@ int main()
 {
 FILE *fr=fopen("/home/km/practice/python/files/File.txt","r+");
 char ch;int i=0;
-fseek(fr,0,SEEK_END);
+fseek(fr,0,SEEK_CUR);
 while((ch=getchar())!=EOF)
 {
 	fputc(ch,fr);
